6710b2efaa51429272e0a9cc68cb4b826a35c6dc
grogu01 <Guenter.Grossberger@ca.com>
2018-06-07 17:10:32 -0700

    add date to thread dump file name

--- thread_dump.sh
+++ thread_dump.sh
@@ -10,10 +10,13 @@ source ./environment.properties
 # is EM running?
 if [ -f "$EM_PATH/em.pid" ]
 then
-        #echo "kill -3 "`cat $EM_PATH/em.pid` " at " `date`
-        kill -3 `cat $EM_PATH/em.pid`
-        if [ "$THREAD_DUMP_JSTACK" ]; then
-            $THREAD_DUMP_JSTACK `cat $EM_PATH/em.pid` >> $EM_PATH/logs/$THREAD_DUMP_FILE
-        fi
+    EM_PID=`cat $EM_PATH/em.pid`
+    #echo "kill -3 "`cat $EM_PATH/em.pid` " at " `date`
+    kill -3 $EM_PID
+    DATE=`date "+%Y%m%d-%H%M%S"`
+    DATED_THREAD_DUMP_FILE=`echo "$THREAD_DUMP_FILE" | sed -E "s#(.*)\.([a-zA-Z]+)#\1.${DATE}.\2#"`
+    if [ "$THREAD_DUMP_JSTACK" ]; then
+        $THREAD_DUMP_JSTACK $EM_PID >> $DATED_THREAD_DUMP_FILE
         #echo "Jstack thread dump at " `date`
+    fi
 fi
